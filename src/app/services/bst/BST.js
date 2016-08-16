2ebdef09efc1ad2856c5e2c3f21731ebefac97d9
Sam Lau <esayemm@gmail.com>
2016-08-15 20:24:50 -0700

    Add BST

--- src/app/services/bst/BST.js
+++ src/app/services/bst/BST.js
@@ -1,5 +1,6 @@
 class Node {
-  constructor(value) {
+  constructor(key, value) {
+    this.key = key
     this.value = value
     this.parent
     this.left
@@ -16,12 +17,139 @@ class BST {
     this.root
   }
 
-  insert(value) {
-    const node = new Node(value)
+  find(key) {
+    const node = this._findNode(key)
+    return node && node.value
+  }
+
+  _findNode(key, node=this.root) {
+    if (!node) return
+    if (node.key === key) return node
+
+    return key < node.key
+      ? this._findNode(key, node.left)
+      : this._findNode(key, node.right)
+  }
+
+  findMin() {
+    let cursor = this.root
+
+    while(cursor.left) {
+      cursor = cursor.left
+    }
+
+    return cursor.value
+  }
+
+  findMax() {
+    let cursor = this.root
+
+    while(cursor.right) {
+      cursor = cursor.right
+    }
+
+    return cursor.value
+  }
+
+  inorder(cb, node=this.root) {
+    if (!node) return []
+
+    const leftArr = this.inorder(cb, node.left || false)
+    const midItem = cb ? cb(node.value) : node.value
+    const rightArr = this.inorder(cb, node.right || false)
+
+    return leftArr.concat(midItem).concat(rightArr)
+  }
+
+  insert(key, value) {
+    const node = new Node(key, value)
+
+    if (!this.root) {
+      this.root = node
+      this.size++
+      return
+    }
 
     let cursor = this.root
     while(cursor) {
+      if (key < cursor.key) {
+        if (!cursor.left) {
+          cursor.left = node
+          node.parent = cursor
+          break
+        }
+        cursor = cursor.left
+      } else {
+        if (!cursor.right) {
+          cursor.right = node
+          node.parent = cursor
+          break
+        }
+        cursor = cursor.right
+      }
+    }
 
+    this.size++
+  }
+
+  remove(key) {
+    const node = this._findNode(key)
+    if (!node) return
+    const retValue = node.value
+    this.size--
+
+    if (node.left && node.right) {
+      let cursor = node.right
+
+      if (cursor.left) {
+        while(cursor.left) {
+          cursor = cursor.left
+        }
+
+        if (!cursor.right) {
+          node.value = cursor.value
+          node.key = cursor.key
+          cursor.parent.left = undefined
+          return retValue
+        } else {
+          node.value = cursor.value
+          node.key = cursor.key
+          cursor.parent.left = cursor.right
+          cursor.right.parent = cursor.parent
+          return retValue
+        }
+      } else {
+        node.value = cursor.value
+        node.key = cursor.key
+        node.right = cursor.right
+        if (cursor.right) {
+          cursor.right.parent = node
+        }
+        return retValue
+      }
+    } else if (node.left) {
+      node.value = node.left.value
+      node.key = node.left.key
+      node.right = node.left.right
+      node.left = node.left.left
+      return retValue
+    } else if (node.right) {
+      node.value = node.right.value
+      node.key = node.right.key
+      node.left = node.right.left
+      node.right = node.right.right
+      return retValue
+    } else {
+      if (!node.parent) {
+        this.root = undefined
+      } else {
+        if (node.parent.left === node) {
+          node.parent.left = undefined
+        } else if (node.parent.right === node) {
+          node.parent.right = undefined
+        }
+      }
+      return retValue
     }
   }
 }
--- src/app/services/bst/bst.spec.js
+++ src/app/services/bst/bst.spec.js
@@ -2,8 +2,154 @@ import BST from './BST.js'
 import expect from 'expect'
 
 describe('BST', () => {
-  it('should work', async () => {
+  it('insert', async () => {
     const bst = new BST()
 
+    expect(bst.find()).toNotExist()
+    bst.insert(5, 'foo')
+    expect(bst.size).toBe(1)
+    expect(bst.find(5)).toBe('foo')
+
+    bst.insert(2, 'bar')
+    expect(bst.size).toBe(2)
+    expect(bst.root.left.key).toBe(2)
+    expect(bst.root.left.parent.key).toBe(5)
+    expect(bst.find(2)).toBe('bar')
+
+    bst.insert(10, 'zed')
+    expect(bst.size).toBe(3)
+    expect(bst.root.right.key).toBe(10)
+    expect(bst.root.right.parent.key).toBe(5)
+    expect(bst.find(10)).toBe('zed')
+
+    bst.insert(1, 'one')
+    expect(bst.size).toBe(4)
+    expect(bst.root.left.left.key).toBe(1)
+  })
+
+  it('findMin', async () => {
+    const bst = new BST()
+    bst.insert(5, 5)
+    bst.insert(2, 2)
+    bst.insert(10, 10)
+    bst.insert(1, 1)
+
+    expect(bst.findMin()).toBe(1)
+  })
+
+  it('findMax', async () => {
+    const bst = new BST()
+    bst.insert(5, 5)
+    bst.insert(2, 2)
+    bst.insert(10, 10)
+    bst.insert(1, 1)
+
+    expect(bst.findMax()).toBe(10)
+  })
+
+  it('inorder', async () => {
+    const bst = new BST()
+    bst.insert(5, 5)
+    bst.insert(2, 2)
+    bst.insert(10, 10)
+    bst.insert(1, 1)
+
+    const arr = bst.inorder()
+    expect(arr[0]).toBe(1)
+    expect(arr[1]).toBe(2)
+    expect(arr[2]).toBe(5)
+    expect(arr[3]).toBe(10)
+
+    const arr2 = bst.inorder(val => val+1)
+    expect(arr2[0]).toBe(2)
+    expect(arr2[1]).toBe(3)
+    expect(arr2[2]).toBe(6)
+    expect(arr2[3]).toBe(11)
+  })
+
+  describe('remove', () => {
+    it('single node', async () => {
+      const bst = new BST()
+      bst.insert(1, 1)
+      expect(bst.remove(1)).toBe(1)
+      expect(bst.root).toNotExist()
+      expect(bst.size).toBe(0)
+    })
+
+    it('leaf node (no children)', async () => {
+      const bst = new BST()
+      bst.insert(2, 2)
+      bst.insert(1, 1)
+      bst.insert(3, 3)
+
+      expect(bst.remove(1)).toBe(1)
+      expect(bst.root.value).toBe(2)
+      expect(bst.root.left).toNotExist()
+      expect(bst.root.right.value).toBe(3)
+    })
+
+    it('node with only left child', async () => {
+      const bst = new BST()
+      bst.insert(3, 3)
+      bst.insert(2, 2)
+      bst.insert(1, 1)
+      bst.insert(4, 4)
+
+      expect(bst.remove(2)).toBe(2)
+      expect(bst.root.left.value).toBe(1)
+      expect(bst.root.left.parent.value).toBe(3)
+      expect(bst.size).toBe(3)
+    })
+
+    it('node with two child', async () => {
+      const bst = new BST()
+      bst.insert(2, 2)
+      bst.insert(1, 1)
+      bst.insert(3, 3)
+
+      expect(bst.remove(2)).toBe(2)
+      expect(bst.root.value).toBe(3)
+      expect(bst.root.left.value).toBe(1)
+      expect(bst.root.left.parent.value).toBe(3)
+      expect(bst.size).toBe(2)
+    })
+
+    /*
+     *       5
+     *    2     10
+     *  1   4
+     *     3
+     */
+    it('complicated tree', async () => {
+      const bst = new BST()
+      bst.insert(5, 5)
+      bst.insert(2, 2)
+      bst.insert(4, 4)
+      bst.insert(3, 3)
+      bst.insert(10, 10)
+      bst.insert(1, 1)
+
+      expect(bst.root.left.left.value).toBe(1)
+      expect(bst.root.left.right.left.value).toBe(3)
+      expect(bst.root.left.right.value).toBe(4)
+      expect(bst.root.left.value).toBe(2)
+      expect(bst.root.value).toBe(5)
+      expect(bst.root.right.value).toBe(10)
+
+      /*
+       *       5
+       *    3     10
+       *  1   4
+       */
+      bst.remove(2)
+      expect(bst.root.left.left.value).toBe(1)
+      expect(bst.root.left.right.value).toBe(4)
+      expect(bst.root.left.right.left).toNotExist()
+      expect(bst.root.left.right.right).toNotExist()
+      expect(bst.root.left.value).toBe(3)
+      expect(bst.root.left.left.value).toBe(1)
+      expect(bst.root.left.right.value).toBe(4)
+      expect(bst.root.value).toBe(5)
+    })
   })
 })
